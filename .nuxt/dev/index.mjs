aa178a88753e475378f210f534221a35e1fa1e85
Neel Dandiwala <neeldandi@gmail.com>
2026-02-22 16:16:04 -0500

    settings

--- .nuxt/dev/index.mjs
+++ .nuxt/dev/index.mjs
@@ -2142,7 +2142,22 @@ const plugins = [
 _pN3sR1KYRLf7WEaYioGEBYmx5_XH1Au435hlEKKQAl8
 ];
 
-const assets = {};
+const assets = {
+  "/index.mjs": {
+    "type": "text/javascript; charset=utf-8",
+    "etag": "\"19ea8-1lMq2WwXed27iLF8GORGlzMvhPc\"",
+    "mtime": "2026-02-22T21:01:20.218Z",
+    "size": 106152,
+    "path": "index.mjs"
+  },
+  "/index.mjs.map": {
+    "type": "application/json",
+    "etag": "\"69c3e-wfFfsChsNsHbPNuuzxyKxLIylHk\"",
+    "mtime": "2026-02-22T21:01:20.218Z",
+    "size": 433214,
+    "path": "index.mjs.map"
+  }
+};
 
 function readAsset (id) {
   const serverDir = dirname$1(fileURLToPath(globalThis._importMeta_.url));
